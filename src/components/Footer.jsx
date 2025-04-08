0797b3652fdf262ccb904e9fdb9b2776121a0a19
000ashishYadav <000ashishyadav2003@gmail.com>
2025-04-08 06:35:04 +0530

    some work done in footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center items-center h-20 bg-red-300 text-white font-medium">
+      Deployed & Managed by Ashish Yadav Student's of Inflection org pvt ltd
+    </div>
+  );
 }
 
 export default Footer;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="h-screen flex justify-center items-center text-2xl">
+      404 || Page Not Found
+    </div>
+  );
 }
 
 export default NotFoundPage;
