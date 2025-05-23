33b8252cc884c97a3184b24fba03095fb84cc347
000ashishYadav <000ashishyadav2003@gmail.com>
2025-05-23 10:54:15 +0530

    apiClient file implemented which helps us for API calling

---
+++ src/utils/apiClient.jsx
@@ -0,0 +1,46 @@
+import { getCookie } from "./cookies";
+
+export const apiClient = async (url, method, body = null, tokenName = "") => {
+  const accessToken = getCookie("access_token");
+
+  const defaultHeaders = {
+    "Content-Type": "application/json",
+  };
+
+  if (tokenName) {
+    defaultHeaders["Authorization"] = `Bearer ${getCookie(tokenName)}`;
+  } else {
+    defaultHeaders["Authorization"] = `Bearer ${accessToken}`;
+  }
+
+  let config;
+
+  if (!body || body === null) {
+    config = {
+      method: method,
+      headers: {
+        ...defaultHeaders,
+      },
+    };
+  } else {
+    config = {
+      method: method,
+      headers: {
+        ...defaultHeaders,
+      },
+      body: JSON.stringify(body),
+    };
+  }
+
+  const response = await fetch(`${baseURL}${url}`, config);
+  return await response.json();
+};
+
+// Auth APIs
+apiClient.login = (payload) => apiClient("/users/login", "POST", payload);
+
+apiClient.refreshAccessToken = () =>
+  apiClient("/users/renew_token", "GET", null, "refresh_token");
+
+// User APIs
+apiClient.fetchMyProfile = () => apiClient("/users/profiles/my", "GET");
